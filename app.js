3e707fed19b3f9264a3cfe7b1ae1320ee75509f8
Xing Ern <lxingern@gmail.com>
2022-12-14 12:39:34 +0800

    print weather conditions from API response

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const request = require('request')
 
 const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=37.8267,-122.4233`
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current)
+    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
 })
