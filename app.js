5dc7a769baaef62bc9ddf63c9458bdc56cfcc493
Xing Ern <lxingern@gmail.com>
2022-12-17 15:53:14 +0800

    implement callback function for weather forecast API and move into utils folder

--- app.js
+++ app.js
@@ -1,33 +1,14 @@
 require('dotenv').config()
-const request = require('request')
 const geocode = require('./utils/geocode')
-
-// const geocoding_url = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.MAPBOX_API_KEY}&limit=1`
-
-// request({ url: geocoding_url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to the location service.')
-//     } else if (response.body.features.length === 0) {
-//         console.log('Unable to find location.')
-//     } else {
-//         console.log('Latitude is ' + response.body.features[0].center[1])
-//         console.log('Longitude is ' + response.body.features[0].center[0])
-//     }
-// })
-
-// const weather_url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_API_KEY}&query=37.8267,-122.4233`
-
-// request({ url: weather_url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather service.')
-//     } else if (response.body.error) {
-//         console.log('Unable to find location.')
-//     } else {
-//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
-//     }
-// })
+const forecast = require('./utils/forecast')
 
 geocode('Philadelphia', (error, data) => {
     console.log(error)
     console.log(data)
 })
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+  })
+  
